55ead8864ef2f13a16083f6987435b3c29e2d510
imkritesh <kritesh94semwal.ks@gmail.com>
2015-07-28 22:59:44 +0530

    config changed 2

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/Project/';
+$config['base_url'] = 'https://project-manager-utility.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
